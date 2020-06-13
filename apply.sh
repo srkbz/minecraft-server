db1c09179a91e2028aee324e97d7e08e3619a885
Carlos Fdez. Llamas <hello@sirikon.me>
2020-06-13 22:44:04 +0200

    Don't use sudo. New ourcraft one-liner installer

--- apply.sh
+++ apply.sh
@@ -4,14 +4,10 @@ cd "$(realpath $(dirname ${BASH_SOURCE[0]}))"
 source "./src/common.sh"
 
 configPath="./config.env"
-tempFolder="./tmp"
 aptDependencies="curl gettext-base git caddy"
 
 function main {
-    create-temp-folder
     load-config
-
-    ensure-sudo
 
     install-apt-repositories \
         "deb [trusted=yes] https://apt.fury.io/caddy/ /"
@@ -20,7 +16,7 @@ function main {
         curl gettext-base git caddy
 
     install-netdata
-    #install-ourcraft
+    install-ourcraft
 
     apply-ufw \
         "default allow outgoing" \
@@ -33,86 +29,65 @@ function main {
     configure-caddy
 
     create-ourcraft-user
-
-    destroy-temp-folder
-}
-
-function create-temp-folder {
-    sudo mkdir -p "$tempFolder"
-    sudo chown -R $(whoami) "$tempFolder"
-}
-
-function destroy-temp-folder {
-    sudo rm -rf "$tempFolder"
 }
 
 function install-apt-repositories {
     log-title "Installing required apt repositories"
     printf " - %s\n" "$@"
-    printf "%s\n" "$@" > "${tempFolder}/srkbz.list"
-    sudo mv "${tempFolder}/srkbz.list" /etc/apt/sources.list.d/srkbz.list
-    sudo chown root:root /etc/apt/sources.list.d/srkbz.list
-    run-silent sudo apt-get update
+    printf "%s\n" "$@" > "/etc/apt/sources.list.d/srkbz.list"
+    run-silent apt-get update
 }
 
 function install-apt-packages {
     log-title "Installing required apt packages"
     printf " - %s\n" $@
-    run-silent sudo apt-get install -y $@
+    run-silent apt-get install -y $@
 }
 
 function install-netdata {
     log-title "Installing netdata"
-    run-silent sudo bash -c 'bash <(curl -Ss https://my-netdata.io/kickstart-static64.sh) --stable-channel --accept'
+    run-silent bash -c 'bash <(curl -Ss https://my-netdata.io/kickstart-static64.sh) --stable-channel --accept'
 }
 
-function install-ourcraft {(
+function install-ourcraft {
     log-title "Installing ourcraft"
-    cd /usr/local/src
-    if [ ! -d "./ourcraft" ]; then
-        run-silent sudo git clone https://github.com/sirikon/ourcraft.git
-    fi
-    cd ./ourcraft
-    run-silent sudo git pull
-    run-silent sudo ./install.sh
-)}
+    run-silent bash -c 'curl -s https://raw.githubusercontent.com/sirikon/ourcraft-installer/master/install.sh | bash'
+}
 
 function apply-ufw {(
     log-title "Installing UFW configuration"
-    run-silent sudo ufw --force reset
-    sudo rm /etc/ufw/*rules.*
+    run-silent ufw --force reset
+    rm /etc/ufw/*rules.*
     for rule in "$@"
     do
-        run-silent sudo ufw $rule
+        run-silent ufw $rule
     done
-    run-silent sudo ufw --force enable
+    run-silent ufw --force enable
 )}
 
 function configure-caddy {
     log-title "Configuring Caddy"
-    envsubst < ./assets/caddyfile.base > "${tempFolder}/Caddyfile"
-    sudo mv "${tempFolder}/Caddyfile" /etc/caddy/Caddyfile
-    sudo chown root:root /etc/caddy/Caddyfile
+    envsubst < ./assets/caddyfile.base > "/etc/caddy/Caddyfile"
     run-silent systemctl reload caddy
     printf "\n"
 }
 
 function create-ourcraft-user {
     username="ourcraft"
-    getent group "${username}" &>/dev/null || sudo groupadd "${username}"
-    id -u "${username}" &>/dev/null || sudo useradd \
+    getent group "${username}" &>/dev/null || groupadd "${username}"
+    id -u "${username}" &>/dev/null || useradd \
         --gid "${username}" \
         --create-home \
         --shell /usr/bin/bash \
         "${username}"
-    sudo loginctl enable-linger "${username}"
+    loginctl enable-linger "${username}"
 
-    sudo mkdir -p ~ourcraft/.ssh
+    mkdir -p ~ourcraft/.ssh
     (
         cd ~ourcraft/.ssh
         cp ~/.ssh/authorized_keys .
-        sudo chown -R "${username}:${username}" ~ourcraft/.ssh
-        sudo chmod +r ~ourcraft/.ssh/authorized_keys
+        chown -R "${username}:${username}" ~ourcraft/.ssh
+        chmod +r ~ourcraft/.ssh/authorized_keys
     )
 }
 
